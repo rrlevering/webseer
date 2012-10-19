a4720c855343b7209732551e323ada1344d21da1
RRLevering <RRLevering@localhost>
2012-10-18 22:12:52 -0400

    Test committing while improving a javadoc comment.

--- src/main/java/org/webseer/model/program/WorkspaceBucketNode.java
+++ src/main/java/org/webseer/model/program/WorkspaceBucketNode.java
@@ -8,7 +8,7 @@ import org.webseer.model.WorkspaceBucket;
 import org.webseer.transformation.TransformationFactory;
 
 /**
- * Workspace buckets are special cases because they have direct access to workspace buckets and in scope act like a
+ * Workspace bucket nodes are special cases because they have direct access to workspace buckets and in scope act like a
  * single transformation. Otherwise, they write out items that pass on their scope chains from when they were written.
  * 
  */
